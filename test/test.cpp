abf38602eda5921a0b84462c2e5985f7f268de2a
Pavel Korozevtsev <paxakor@yandex.ru>
2016-04-21 17:09:31 +0300

    tester works

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "../lib/inttypes.hpp"
 
-int main() {
-  uint32_t u, v;
-  std::cin >> u >> v;
+int main(int argc, char const *argv[]) {
+  uint64_t u, v;
+  if (argc > 2) {
+    u = std::stoull(argv[1]);
+    v = std::stoull(argv[2]);
+  } else {
+    std::cin >> u >> v;
+  }
   const uint24_t a(u);
   const uint24_t b(v);
   std::cout << std::to_string(a + b, 2) << std::endl
--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
 from random import randint
-from subprocess import run
+from subprocess import Popen, PIPE
 from sys import argv
 
 n = 1000
 s = 24
+l = 24
 
 if "-n" in argv:
   n = int(argv[argv.index("-n") + 1])
@@ -14,4 +15,21 @@ errors = 0
 for i in range(n):
   a = randint(0, 2**s - 1)
   b = randint(0, 2**s - 1)
-  run(["./test"])
+  proc = Popen(["./test", str(a), str(b)], stdout = PIPE, stderr = PIPE)
+  proc.wait()
+  out, err = proc.communicate()
+  out = out.split()
+  if int(out[0], 2) != a + b:
+    print(a, '+', b, '!=', int(out[0], 2))
+    errors += 1
+  if int(out[1], 2) != a - b and int(out[1], 2) != 2**l + (a - b):
+    print(a, '-', b, '!=', int(out[1], 2))
+    errors += 1
+  if int(out[2], 2) != a * b:
+    print(a, '*', b, '!=', int(out[2], 2))
+    errors += 1
+  if int(out[3], 2) != a // b:
+    print(a, '/', b, '!=', int(out[3], 2))
+    errors += 1
+
+print(errors)
