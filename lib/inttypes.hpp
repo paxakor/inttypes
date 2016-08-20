4e4f3e1bfff007850c87b8309a276d1d302c88c9
Pavel Korozevtsev <paxakor@yandex.ru>
2016-08-20 14:52:05 +0300

    typedef -> using

--- lib/inttypes.hpp
+++ lib/inttypes.hpp
@@ -4,18 +4,18 @@
 
 namespace pkr {
 
-typedef Uint<uint8_t,  uint16_t> uint24_t;
-typedef Uint<uint8_t,  uint32_t> uint40_t;
-typedef Uint<uint16_t, uint32_t> uint48_t;
-typedef Uint<uint8_t,  uint48_t> uint56_t;
-typedef Uint<uint8_t,  uint64_t> uint72_t;
-typedef Uint<uint16_t, uint64_t> uint80_t;
-typedef Uint<uint8_t,  uint80_t> uint88_t;
-typedef Uint<uint32_t, uint64_t> uint96_t;
-typedef Uint<uint40_t, uint64_t> uint104_t;
-typedef Uint<uint64_t, uint64_t> uint128_t;
-typedef Uint<uint128_t, uint128_t> uint256_t;
-typedef Uint<uint256_t, uint256_t> uint512_t;
-typedef Uint<uint512_t, uint512_t> uint1024_t;
+using uint24_t   = Uint<uint8_t,  uint16_t>;
+using uint40_t   = Uint<uint8_t,  uint32_t>;
+using uint48_t   = Uint<uint16_t, uint32_t>;
+using uint56_t   = Uint<uint8_t,  uint48_t>;
+using uint72_t   = Uint<uint8_t,  uint64_t>;
+using uint80_t   = Uint<uint16_t, uint64_t>;
+using uint88_t   = Uint<uint8_t,  uint80_t>;
+using uint96_t   = Uint<uint32_t, uint64_t>;
+using uint104_t  = Uint<uint40_t, uint64_t>;
+using uint128_t  = Uint<uint64_t, uint64_t>;
+using uint256_t  = Uint<uint128_t, uint128_t>;
+using uint512_t  = Uint<uint256_t, uint256_t>;
+using uint1024_t = Uint<uint512_t, uint512_t>;
 
 }  // namespace pkr
